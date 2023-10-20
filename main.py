4548eda98db440bb804a5ffdbe13b39318d0b2e8
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-20 19:22:43 +0700

    "Improve homepage UI and correct API routes
    Modified the HTML for the homepage to include a more sophisticated styling and added Bootstrap CSS for better visuals and user experience. The change includes converting list items into clickable buttons and adding a Docs button. Corrected API routes to include preceding '/api/' for better viewing and organization on the homepage."

--- main.py
+++ main.py
@@ -34,38 +34,59 @@ class Ayat(BaseModel):
     nomor: str
     tr: str
 
+
 @app.get("/", response_class=HTMLResponse)
 def read_home():
     return """
-    <html>
+    <html style="height: 100%;">
         <head>
             <title>Muslim API</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+            <style>
+                .hero {
+                    height: 100%;
+                    background: #007BFF;
+                    color: white;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    flex-direction: column;
+                    text-align: center;
+                }
+                .hero a {
+                    color: black;
+                    margin: 5px;
+                }
+                .btn-light {
+                    color: #fff;
+                }
+            </style>
         </head>
         <body>
-          <h1>Welcome to Muslim API</h1>
-          <p>
-            A REST API that provides access to data related to Asmaul Husna and Quran.
-          </p>
-          <ul>
-            <li><a href="/api/asmaul-husna">Asmaul Husna</a></li>
-            <li><a href="/api/quran">Quran Surah</a></li>
-          </ul>
+            <div class="hero">
+                <h1>Welcome to Muslim API</h1>
+                <p>A REST API that provides access to data related to Asmaul Husna and Quran.</p>
+                <a href="/api/asmaul-husna" class="btn btn-light">Asmaul Husna</a>
+                <a href="/api/quran" class="btn btn-light">Quran Surah</a>
+                <a href="/docs" class="btn btn-light">Docs</a>
+            </div>
         </body>
     </html>
     """
+
 @app.get("/api/")
 def read_root():
-    return {"Title" : "Muslim API"}
+    return {"Title": "Muslim API"}
 
 
-@app.get("api/asmaul-husna")
+@app.get("/api/asmaul-husna")
 async def root():
     with open('data/asmaul-husna.json') as f:
         data = json.load(f)
     return data
 
 
-@app.get("api/asmaul-husna/random", response_model=Data)
+@app.get("/api/asmaul-husna/random", response_model=Data)
 async def read_random_asmaul_husna():
     with open('data/asmaul-husna.json') as f:
         data = json.load(f)
@@ -74,7 +95,7 @@ async def read_random_asmaul_husna():
     return {"arabic": data[random_id]['arab'], "latin": data[random_id]['latin'], "arti": data[random_id]['artinya']}
 
 
-@app.get("api/asmaul-husna/{id}", response_model=Data)
+@app.get("/api/asmaul-husna/{id}", response_model=Data)
 async def read_asmaul_husna(id: str):
     with open('data/asmaul-husna.json') as f:
         data = json.load(f)
@@ -85,7 +106,7 @@ async def read_asmaul_husna(id: str):
         raise HTTPException(status_code=404, detail="Not found")
 
 
-@app.get("api/quran", response_model=List[QuranData])
+@app.get("/api/quran", response_model=List[QuranData])
 def get_quran():
     with open('data/quran.json') as file:
         data = json.load(file)
@@ -93,7 +114,7 @@ def get_quran():
         return data
 
 
-@app.get("api/quran/{nomor}")
+@app.get("/api/quran/{nomor}")
 def get_quran_nor(nomor: str):
     with open('data/quran.json') as file:
         data = json.load(file)
@@ -105,7 +126,7 @@ def get_quran_nor(nomor: str):
     return {"message": "Not found"}
 
 
-@app.get("api/quran/{nomor}/detail", response_model=List[Ayat])
+@app.get("/api/quran/{nomor}/detail", response_model=List[Ayat])
 def read_quran_detail(nomor: str):
     file_path = f"data/surah/{nomor}.json"
     if os.path.exists(file_path):
