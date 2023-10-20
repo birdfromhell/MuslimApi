69cd2ff239d09a1144164f23aa5607addd0defd2
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-20 19:06:52 +0700

    "Delete doa.py file
    The doa.py file has been removed. The file was used to store data and metadata for each type of Doa. However, this data has been moved to a database which significantly improves the way we handle, manage, and retrieve this information. Therefore, the doa.py file was no longer required and has been deleted to declutter the codebase."

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
+import os
 import random
 from typing import List
 
+import requests
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
@@ -27,6 +29,13 @@ class QuranData(BaseModel):
     urut: str
 
 
+class Ayat(BaseModel):
+    ar: str
+    id: str
+    nomor: str
+    tr: str
+
+
 @app.get("/")
 def read_root():
     return {"Title : Muslim API"}
@@ -34,14 +43,14 @@ def read_root():
 
 @app.get("/asmaul-husna")
 async def root():
-    with open('asmaul-husna.json') as f:
+    with open('data/asmaul-husna.json') as f:
         data = json.load(f)
     return data
 
 
 @app.get("/asmaul-husna/random", response_model=Data)
 async def read_random_asmaul_husna():
-    with open('asmaul-husna.json') as f:
+    with open('data/asmaul-husna.json') as f:
         data = json.load(f)
 
     random_id = random.choice(list(data.keys()))
@@ -50,7 +59,7 @@ async def read_random_asmaul_husna():
 
 @app.get("/asmaul-husna/{id}", response_model=Data)
 async def read_asmaul_husna(id: str):
-    with open('asmaul-husna.json') as f:
+    with open('data/asmaul-husna.json') as f:
         data = json.load(f)
 
     if id in data:
@@ -61,7 +70,7 @@ async def read_asmaul_husna(id: str):
 
 @app.get("/quran", response_model=List[QuranData])
 def get_quran():
-    with open('quran.json') as file:
+    with open('data/quran.json') as file:
         data = json.load(file)
 
         return data
@@ -69,7 +78,7 @@ def get_quran():
 
 @app.get("/quran/{nomor}")
 def get_quran_nor(nomor: str):
-    with open('quran.json') as file:
+    with open('data/quran.json') as file:
         data = json.load(file)
 
     for item in data:
@@ -77,3 +86,14 @@ def get_quran_nor(nomor: str):
             return item
 
     return {"message": "Not found"}
+
+
+@app.get("/quran/{nomor}/detail", response_model=List[Ayat])
+def read_quran_detail(nomor: str):
+    file_path = f"data/surah/{nomor}.json"
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    else:
+        raise HTTPException(status_code=404, detail="Surah not found")
